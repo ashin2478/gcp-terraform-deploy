a0214bf1f931c56a0d89b577246771dc4ef25666
ashin2478 <ashinpulikken007@gmail.com>
2025-02-12 14:00:15 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,6 @@
-provider "google" {
-  project = "your-project-id"
-  region  = "us-central1"
-}
-
-resource "google_storage_bucket" "example" {
-  name     = "my-secure-bucket"
-  location = "US"
-
-  versioning {
-    enabled = true
+terraform {
+  backend "gcs" {
+    bucket  = "your-terraform-state-bucket"
+    prefix  = "terraform/state"
   }
-}
-
-resource "google_project_iam_member" "bucket_access" {
-  project = "your-project-id"
-  role    = "roles/storage.admin"
-  member  = "user:your-email@gmail.com"
 }
